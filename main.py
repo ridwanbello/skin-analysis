bf7debf19d4eeddab2dcc61628f0e98690e78dfd
ridwanbello <belloridwan60@gmail.com>
2026-05-03 23:46:20 -0500

    add CORS middleware

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ load_dotenv()
 from fastapi import FastAPI
 from routes import skin, weather, routine
 import os
+from fastapi.middleware.cors import CORSMiddleware
+
 
 PERFECTCORP_API_KEY = os.getenv("PERFECTCORP_API_KEY")
 OPENWEATHERMAP_API_KEY = os.getenv("OPENWEATHERMAP_API_KEY")
@@ -11,6 +13,14 @@ OPENWEATHERMAP_API_KEY = os.getenv("OPENWEATHERMAP_API_KEY")
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # restrict to your frontend URL in production
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(skin.router, prefix="/api/skin")
 app.include_router(weather.router, prefix="/api/weather")
